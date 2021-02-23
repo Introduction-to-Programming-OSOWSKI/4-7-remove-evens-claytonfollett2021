d60c256a737c5c6cb04c15b5d337592d8dc6bf48
claytonfollett2021 <813551@mystma.org>
2021-02-23 16:19:17 +0000

    removeEvens

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#WRITE YOUR CODE IN THIS FILE
+def removeEvens(k):
+    newlist = k 
+    numPopped = 0
+    for i in range (0, len(k)):
+       if newlist[i - numPopped] % 2 == 0 and newlist[i - numPopped] != 0:
+           newlist.pop(i - numPopped)
+           numPopped = numPopped + 1 
+
+    return newlist
+
+print(removeEvens([1, 2, 3, 4, 5, 6, 7, 8, 9,]))
+    
